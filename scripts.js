6ec09e8cbce4dd29d4d3fbba3e8a5e72dfad522c
Dhairya Saluja <dhairyarsaluja@gmail.com>
2026-08-22 21:50:31 +0400

    added smooth scrolling

--- scripts.js
+++ scripts.js
@@ -8,3 +8,11 @@ let projects = [
 for (let i = 0; i < projects.length; i++) {
   console.log(projects[i]);
 }
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
